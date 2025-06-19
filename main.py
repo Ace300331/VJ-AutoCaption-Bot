42ca7e61923616c846abf5ecc2511fd904bd4440
Ace300331 <kujoora923@gmail.com>
2025-06-19 20:22:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,26 @@
 
 import pyrogram, os, asyncio
 
-app_id = int(os.environ.get("app_id", ""))
-api_hash = os.environ.get("api_hash", "")
-bot_token = os.environ.get("bot_token", "")
-custom_caption = os.environ.get("custom_caption", "`{file_name}`\n\n**Powered By - @VJ_Botz**") # Here You Can Give Anything, if You Want Real File Name Then Use {file_name}
+app_id = int(os.environ.get("app_id", "23790461"))
+api_hash = os.environ.get("api_hash", "2d0a7fb85f06246e93948bf7afc05fb3")
+bot_token = os.environ.get("bot_token", "8140367385:AAEkE6mkxoPwUTRON1XJY4OD9hYG9JMCvwA")
+custom_caption = os.environ.get("custom_caption", "`{file_name}`\n\n**✅ Join Us [@Ace_Files]**") # Here You Can Give Anything, if You Want Real File Name Then Use {file_name}
 
 AutoCaptionBotV1 = pyrogram.Client(name="AutoCaptionBotV1", api_id=app_id, api_hash=api_hash, bot_token=bot_token)
 
 start_message = """
 <b>👋Hello {}</b>
-<b>I am an AutoCaption bot</b>
+<b>I am an Ace AutoCaption bot</b>
 <b>All you have to do is to add me to your channel as admin and I will show you my power</b>
-<b>@VJ_Botz</b>"""
+<b>@Ace_Files</b>"""
 
 about_message = """
-<b>• Name : <a href=https://t.me/VJ_Botz>VJ AutoCaption</a></b>
-<b>• Developer : <a href=https://t.me/VJ_Botz>[VJ UPDATES]</a></b>
+<b>• Name : <a href=https://t.me/Ace_Files>Ace AutoCaption</a></b>
+<b>• Developer : <a href=https://t.me/Ace_Files>[Ace UPDATES]</a></b>
 <b>• Language : Python3</b>
 <b>• Library : Pyrogram v{version}</b>
-<b>• Updates : <a href=https://t.me/VJ_Botz>Click Here</a></b>
-<b>• Source Code : <a href=https://github.com/VJBots/VJ-AutoCaption-Bot>Click Here</a></b>"""
+<b>• Updates : <a href=https://t.me/Ace_Files>Click Here</a></b>
+<b>• Source Code : <a href=https://t.me/Ace_Files>Click Here</a></b>"""
 
 @AutoCaptionBotV1.on_message(pyrogram.filters.private & pyrogram.filters.command(["start"]))
 def start_command(bot, update):
@@ -71,10 +71,10 @@ def get_file_details(update: pyrogram.types.Message):
 def start_buttons(bot, update):
     bot = bot.get_me()
     buttons = [[
-        pyrogram.types.InlineKeyboardButton("Updates", url="t.me/VJ_Botz"),
+        pyrogram.types.InlineKeyboardButton("Updates", url="t.me/Ace_Files"),
         pyrogram.types.InlineKeyboardButton("About 🤠", callback_data="about")
     ],[
-        pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{bot.username}?startchannel=true")
+        pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/Ace_Auto_Caption_bot?startchannel=true")
     ]]
     return pyrogram.types.InlineKeyboardMarkup(buttons)
 
@@ -84,8 +84,8 @@ def about_buttons(bot, update):
     ]]
     return pyrogram.types.InlineKeyboardMarkup(buttons)
 
-print("Telegram AutoCaption V1 Bot Start")
-print("Bot Created By @VJ_Botz")
+print("Telegram Ace AutoCaption Bot Start")
+print("Bot Created By @Ace_Files")
 
 AutoCaptionBotV1.run()
 
